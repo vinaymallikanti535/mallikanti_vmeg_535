3ce15b2f3a87bc036774b9088f95c476ee7810a9
vinaymallikanti535 <31734357+vinaymallikanti535@users.noreply.github.com>
2017-09-07 20:36:39 +0530

    Guessing Game

---
+++ GuessingGame.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+class GuessingGame {
+ 
+Scanner s=new Scanner(System.in);
+int n; 
+int m=s.nextInt();  
+public int answer;
+   
+public int min;
+    
+public int max;
+ 
+public GuessingGame(int min, int max) {
+  
+guess();
+	}
+
+    public int guess() {
+ 
+
+	
+
+	 n=s.nextInt();
+
+	if(n==m){
+			System.out.println("Bingo!");
+
+				System.exit(1);
+		}
+		
+	else if(n<m){
+			System.out.println("higher than"+"  "+n);
+
+			higher();
+		    }
+	else
+		{
+			System.out.println("lower than"+n);
+			lower();
+		}	
+
+       				return 0;
+ 
+
+			   }
+
+   
+ 	public void lower() {
+ guess();  
+				}
+
+  
+
+ 
+ public void higher() {
+ guess();
+   
+			}
+
+
+public static void main(String a[])
+{
+GuessingGame game = new GuessingGame(0, 100);
+game.guess();
+}}
